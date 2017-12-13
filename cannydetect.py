77be2fc99e2e16ddbae7f0dab2cc50bd70fd252b
pldl <prestonzlau@gmail.com>
2017-12-12 19:14:32 -0800

    Add files via upload

--- cannydetect.py
+++ cannydetect.py
@@ -9,74 +9,103 @@ import argparse
 import glob
 import imutils
 import cv2
-
+import socket
+def checkEqual1(iterator):
+    iterator = iter(iterator)
+    try:
+        first = next(iterator)
+    except StopIteration:
+        return True
+    return all(first == rest for rest in iterator)
 def auto_canny(image, sigma=0.33):
 	v=np.median(image)
 	lower = int(max(0,(1.0-sigma)*v))
 	upper = int(min(255,(1.0+sigma)*v))
 	edged=cv2.Canny(image,lower,upper)
 	return edged
-
+host=''
+port=50007
+size=1024
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((host,port))
+s.listen(5)
+dup=[]
 Quit=0
 cap = cv2.VideoCapture(0)
-
 while(True):
-	ret,image=cap.read()
-	if image is None:
-		break
-	resized = imutils.resize(image, width=300)
-	ratio = image.shape[0] / float(resized.shape[0])
+	client,address=s.accept()
+	while(True):
+		
+		B=0
+		ret,image=cap.read()
+		if image is None:
+			break
+		resized = imutils.resize(image, width=300)
+		ratio = image.shape[0] / float(resized.shape[0])
 
 # blur the resized image slightly, then convert it to both
 # grayscale and the L*a*b* color spaces
-	blurred = cv2.GaussianBlur(resized, (5, 5), 0)
-	gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
-	auto= auto_canny(blurred)
-	lab = cv2.cvtColor(blurred, cv2.COLOR_BGR2LAB)
-	thresh = cv2.threshold(auto, 100, 255, cv2.THRESH_BINARY)[1]
-	cv2.imshow("Thresh", thresh)
+		blurred = cv2.GaussianBlur(resized, (5, 5), 0)
+		gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
+		auto= auto_canny(blurred)
+		lab = cv2.cvtColor(blurred, cv2.COLOR_BGR2LAB)
+		thresh = cv2.threshold(auto, 100, 255, cv2.THRESH_BINARY)[1]
+		cv2.imshow("Thresh", thresh)
 	
 
 # find contours in the thresholded image
-	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)
-	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+		cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+			cv2.CHAIN_APPROX_SIMPLE)
+		cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 	
         
 # initialize the shape detector and color labeler
-	sd = ShapeDetector()
-	cl = ColorLabeler()
-	Output=""
+		sd = ShapeDetector()
+		cl = ColorLabeler()
+		Output=""
 # loop over the contours
-	for c in cnts:
+		for c in cnts:
 	# compute the center of the contour
-		M = cv2.moments(c)
-		if M["m00"]!=0:
-			cX = int((M["m10"] / M["m00"]) * ratio)
-			cY = int((M["m01"] / M["m00"]) * ratio)
-		else:
-			cX,cY=0,0
+			M = cv2.moments(c)
+			if M["m00"]!=0:
+				cX = int((M["m10"] / M["m00"]) * ratio)
+				cY = int((M["m01"] / M["m00"]) * ratio)
+			else:
+				cX,cY=0,0
 
 	# detect the shape of the contour and label the color
-		shape = sd.detect(c)
-		color = cl.label(lab, c)
+			shape = sd.detect(c)
+			color = cl.label(lab, c)
 
 	# multiply the contour (x, y)-coordinates by the resize ratio,
 	# then draw the contours and the name of the shape and labeled
 	# color on the image
-		c = c.astype("float")
-		c *= ratio
-		c = c.astype("int")
-		text = "{} {}".format(color, shape)
+			c = c.astype("float")
+			c *= ratio
+			c = c.astype("int")
+			text = "{} {}".format(color, shape)
                 #change into specific color shape combinations.
-		if color!="white" and color!="black" and shape!="big" and shape!="small" and shape!="unknown":
-			if Output!="":
-				Output+=" "
-			Output+=text
-	if Output!="":
-		print Output
-	cv2.imshow("Image",image)
-	if cv2.waitKey(1)& 0xFF == ord('q'):
+			if text=="yellow circle" or text=="red pentagon" or text=="green rectangle" or text=="orange triangle":
+				if Output!="":
+					Output+=" "
+				Output+=text
+		cv2.imshow("Image",image)
+		if cv2.waitKey(1)& 0xFF == ord('q'):
+			Quit=1
+			break
+		if Output!="":
+			dup.append(Output)
+			if len(dup)==10:
+				if checkEqual1(dup)==True:
+					client.send(Output+"\n")
+					print Output
+					client.close()
+					B=1
+					Quit=1
+				dup=[]
+		if B==1:
+			break
+	if Quit==1:
 		break
 cap.release()		
 cv2.destroyAllWindows()
--- circle.py
+++ circle.py
@@ -2,48 +2,82 @@ import numpy as np
 import argparse
 import imutils
 import cv2
+import socket
+
 
 cap = cv2.VideoCapture(0)
+def reject_outliers(data, m = 2.):
+    d = np.abs(data - np.median(data))
+    mdev = np.median(d)
+    s = d/mdev if mdev else 0.
+    return data[s<m]
+def avg(l):
+    return sum(l)/float(len(l))
+
+distance=[]
+adistance=0
+q=0
+
+host=''
+port=50000
+size=1024
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((host,port))
+s.listen(5)
 
 while(True):
-	ret,image=cap.read()
-	if image is None:
-		break
-	output=image.copy()
-	hsv=cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
-	lower_red=np.array([0,126,100])
-	upper_red=np.array([15,255,255])
-	maskO = cv2.inRange(hsv, lower_red,upper_red)
-	oRes=cv2.bitwise_and(image,image,mask=maskO)
-	cv2.imshow("mask",oRes)
+	client,address=s.accept()
+	while(True):
+		ret,image=cap.read()
+		if image is None:
+			break
+		output=image.copy()
+		hsv=cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+		lower_red=np.array([0,140,125])
+		upper_red=np.array([10,255,255])
+		maskO = cv2.inRange(hsv, lower_red,upper_red)
+		oRes=cv2.bitwise_and(image,image,mask=maskO)
+		cv2.imshow("mask",oRes)
 	# find contours in the mask and initialize the current
 	# (x, y) center of the ball
-	cnts = cv2.findContours(maskO.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)[-2]
-	center = None
+		cnts = cv2.findContours(maskO.copy(), cv2.RETR_EXTERNAL,
+			cv2.CHAIN_APPROX_SIMPLE)[-2]
+		center = None
  
 	# only proceed if at least one contour was found
-	if len(cnts) > 0:
+		if len(cnts) > 0:
 		# find the largest contour in the mask, then use
 		# it to compute the minimum enclosing circle and
 		# centroid
-		c = max(cnts, key=cv2.contourArea)
-		((x, y), radius) = cv2.minEnclosingCircle(c)
-		M = cv2.moments(c)
-		if M["m00"]!=0:
-			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-		else:
-			center = (0,0)
-		# only proceed if the radius meets a minimum size
-		if True:
-			# draw the circle and centroid on the frame,
-			# then update the liqst of tracked points
-			cv2.circle(image, (int(x), int(y)), int(radius),
-				(0, 255, 255), 2)
-			cv2.circle(image, center, 5, (0, 0, 255), -1)
-		print (33/radius)
-	cv2.imshow("frame", image)
-	if cv2.waitKey(1)& 0xFF == ord('q'):
+			c = max(cnts, key=cv2.contourArea)
+			((x, y), radius) = cv2.minEnclosingCircle(c)
+			M = cv2.moments(c)
+			if M["m00"]!=0:
+				center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+			else:
+				center = (0,0)
+			# only proceed if the radius meets a minimum size
+			if True:
+				# draw the circle and centroid on the frame,
+				# then update the liqst of tracked points
+				cv2.circle(image, (int(x), int(y)), int(radius),
+					(0, 255, 255), 2)
+				cv2.circle(image, center, 5, (0, 0, 255), -1)
+				if (33/radius < 15):
+					distance.append(33/radius)
+		cv2.imshow("frame", image)
+		if cv2.waitKey(1)& 0xFF == ord('q'):
+			q=1
+			break		
+		if len(distance)==25:
+			adistance=avg(distance)
+			client.send(str(adistance)+"\n")
+			print(str(adistance))
+			distance=[]
+			client.close()
+			break
+
+	if q==1:
 		break
 cap.release()		
 cv2.destroyAllWindows()
